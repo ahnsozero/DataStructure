666559d5fcf63b2a23e6f4b36772b4f1d704e0e5
ahnsozero <92905478+ahnsozero@users.noreply.github.com>
2022-05-01 05:54:41 +0900

    ArrayList the End

--- src/list/arraylist/implementation/ArrayList.java
+++ src/list/arraylist/implementation/ArrayList.java
@@ -15,12 +15,105 @@ public class ArrayList {
 	}
 	
 	public boolean add(int index, Object element) {
+		System.out.println("너 뭐로전달도돼? " + index);
 		for(int i = size-1; i>=index; i--) {
 			elementData[i+1] = elementData[i];
 		}
 		elementData[index] = element;
 		size++;
 		return true;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "[";
+		for(int i=0; i < size; i++) {
+			str += elementData[i];
+			if(i < size-1) { // 마지막 인덱스 다음엔 콤마를 붙이지 않겠다
+				str += ", "; 	
+			}
+		}
+		return str + "]";
+	}
+	
+	public Object remove(int index) {
+		Object removed = elementData[index]; // 삭제전에 리턴해주기 위해 임시저장하는 변수
+		for(int i = index + 1; i <= size - 1; i++) {
+			elementData[i-1] = elementData[i];
+		}
+		size--;
+		elementData[size] = null;
+		
+		return removed; //삭제한 값을 리턴
+	}
+	
+	public Object removeFirst() {
+		return remove(0);
+	}
+	
+	public Object removeLast() {
+		return remove(size-1); 
+	}
+	
+	public Object get(int index) {
+		return elementData[index];
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int indexOf(Object o) {
+		for(int i = 0; i < size; i++) {
+			if(o.equals(elementData[i])) {
+				return i;
+			}
+		}			
+		return -1; //없는 값일때 반환
+	}
+	
+	public ListIterator listIterator() {
+		return new ListIterator(); //새로운 ListIterator객체를 반환
+	}
+	
+	class ListIterator{
+		private int nextIndex = 0;
+		
+		public Object next() {
+			/*
+			Object returnData = elementData[nextIndex];
+			nextIndex++;
+			return returnData;
+			*/
+			System.out.println(nextIndex);
+			return elementData[nextIndex++]; // 위코드와 동일한 코드
+			
+		}
+		
+		public boolean hasNext() {
+			
+			return nextIndex < size();
+		}
+		
+		public Object previous() {
+			return elementData[--nextIndex];
+		}
+		
+		public boolean hasPrevious() {
+			return nextIndex > 0; // 0과 같다면 --nextIndex일때 -1로 되서 outofIndex 에러뜸
+		}
+		
+		public void add(Object element) {
+			System.out.println(nextIndex);
+			ArrayList.this.add(nextIndex++, element);
+			System.out.println(nextIndex);
+		}
+		
+		public void remove() {
+			ArrayList.this.remove(nextIndex-1);
+			nextIndex--;
+		}
+		
 	}
 	
 
--- src/list/arraylist/implementation/Main.java
+++ src/list/arraylist/implementation/Main.java
@@ -8,8 +8,64 @@ public class Main {
 		numbers.addLast(20);
 		numbers.addLast(30);
 		numbers.addLast(40);		
-		numbers.add(1, 15);
-		numbers.addFirst(5);
+		//numbers.add(1, 15);		
+		//numbers.addFirst(5);		
+		/*
+		System.out.println(numbers.remove(1));
+		System.out.println(numbers);
+		numbers.removeFirst();
+		System.out.println(numbers);
+		numbers.removeLast();
+		System.out.println(numbers);
+		System.out.println(numbers.get(1));
+		System.out.println(numbers.get(0));
+		System.out.println(numbers.size());
+		System.out.println(numbers.indexOf(70));
+		
+		for(int i=0; i<numbers.size(); i++) {
+			System.out.println(numbers.get(i));
+		}
+		*/
+		
+		// 데이터 타입
+		ArrayList.ListIterator li = numbers.listIterator();
+		//System.out.println(li.next());
+		//System.out.println(li.next());
+		//System.out.println(li.next());
+		//System.out.println(li.next());// 가져올 값이 없기 때문에 null
+	
+		/*
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+		
+		while(li.hasPrevious()) {
+			System.out.println(li.previous());
+		}
+		*/
+		
+		// 이거 모르겠다★ 
+		while(li.hasNext()) {
+			System.out.println("뭐야");
+			int number = (int)li.next();			
+			if(number == 20) {
+				System.out.println("이해안가서 찍어봄");
+				li.add(25);
+			}
+		}
+		System.out.println(numbers);
+		
+		/*
+		while(li.hasNext()) {
+			int number = (int)li.next();
+			if(number == 30) {
+				li.remove();
+			}
+			System.out.println(number);
+		}
+		System.out.println(numbers);
+		*/
 	}
+	
 
 }
